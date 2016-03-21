72f8c8a5c0fd92409b6ed7bde177a2de5bef959f
narum <joan@192.168.1.35>
2016-03-21 22:03:31 +0100

    Afegit que els complements de nom que siguin objecte portin article determinat.

--- application/libraries/Myslot.php
+++ application/libraries/Myslot.php
@@ -2581,6 +2581,7 @@ class Myslot {
                                 $wordaux->isClass("vehicle") ||
                                 $wordaux->isClass("human") ||
                                 $wordaux->isClass("event") ||
+                                $wordaux->isClass("objecte") ||
                                 $wordaux->isClass("planta")) $definite = true;
                         else $noarticle = true;
                     }
