52ceae516b62a441d7fd5976a707b6c10e402435
narum <joan@eurk-126-15.uab.es>
2016-06-21 14:24:12 +0200

    Myexpander: Que les frases que no es poden expandir es guardin el generatorString a la BBDD.

--- application/libraries/Myexpander.php
+++ application/libraries/Myexpander.php
@@ -123,7 +123,10 @@ class Myexpander {
         // si ha trobat un pictograma que no es pot expandir
         // o el sistema d'expansió estava desactivat que surti del sistema d'expansió
         if ($this->readwithoutexpansion || $expansionOff) {
+            
             $this->info['frasefinal'] = $frasefinalnotexpanded;
+            $CI->Lexicon->guardarParseIFraseResultat($propietatsfrase['identry'], null, $frasefinalnotexpanded);
+            
             return;
         }
 
@@ -140,6 +143,8 @@ class Myexpander {
             $this->info['readwithoutexpansion'] = $this->readwithoutexpansion;
             
             $this->info['frasefinal'] = $frasefinalnotexpanded;
+            $CI->Lexicon->guardarParseIFraseResultat($propietatsfrase['identry'], null, $frasefinalnotexpanded);
+            
             return;
         }
 
@@ -200,6 +205,7 @@ class Myexpander {
                    $this->info['readwithoutexpansion'] = $this->readwithoutexpansion;
 
                    $this->info['frasefinal'] = $frasefinalnotexpanded;
+                   $CI->Lexicon->guardarParseIFraseResultat($propietatsfrase['identry'], null, $frasefinalnotexpanded);
 
                    return;
                }
@@ -341,7 +347,8 @@ class Myexpander {
 //            }
 
             // Guardar parse tree i frase final a la base de dades
-            $CI->Lexicon->guardarParseIFraseResultat($propietatsfrase['identry'], $printparsepattern, $frasefinal);
+            if ($this->readwithoutexpansion) $CI->Lexicon->guardarParseIFraseResultat($propietatsfrase['identry'], null, $frasefinalnotexpanded);
+            else $CI->Lexicon->guardarParseIFraseResultat($propietatsfrase['identry'], $printparsepattern, $frasefinal);
 
             $this->info['errormessage'] = $this->errormessage[$bestpatternindex];
             $this->info['error'] = $this->error[$bestpatternindex];
