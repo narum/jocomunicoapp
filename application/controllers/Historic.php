51b14d095403bd438dc0fa65fd767fe44b75604d
oscaralonsob <oscaralonsob@gmail.com>
2016-06-22 12:49:11 +0200

    commit para poner lo de xavi

--- application/controllers/Historic.php
+++ application/controllers/Historic.php
@@ -27,4 +27,51 @@ class Historic extends REST_Controller {
         $this->response($response, REST_Controller::HTTP_OK);
     }
 
+    public function getHistoric_post() {
+        $postdata = file_get_contents("php://input");
+        $request = json_decode($postdata);
+        $day = $request->day;
+        $pagHistoric = $request->pagHistoric;
+
+        $historicArray = array();
+        $idusu = $this->session->userdata('idusu');
+        $historic = $this->HistoricInterface->getHistoric($idusu, $day);
+        for ($i = $pagHistoric; $i < count($historic) && $i < $pagHistoric + 10; $i++){
+            $arrayProv[0] = $historic[$i];
+            $arrayProv[1] = $this->HistoricInterface->getPictosHistoric($historic[$i]->ID_SHistoric);
+            array_push($historicArray,$arrayProv);
+        }
+        $count = $this->HistoricInterface->getCountHistoric($idusu, $day);
+
+        $response = [
+            'historic' => $historicArray,
+            'count' => $count
+        ];
+        $this->response($response, REST_Controller::HTTP_OK);
+    }
+    
+    function getFolder_post(){
+        $postdata = file_get_contents("php://input");
+        $request = json_decode($postdata);
+        $idfolder = $request->folder;
+        $pagHistoric = $request->pagHistoric;
+        
+        $sentenceArray = array();
+        $idusu = $this->session->userdata('idusu');
+        $sentenece = $this->HistoricInterface->getSentenceFolder($idusu, $idfolder);
+
+        for ($i = $pagHistoric; $i < count($sentenece) && $i < $pagHistoric + 10; $i++){
+            $arrayProv[0] = $sentenece[$i];
+            $arrayProv[1] = $this->HistoricInterface->getPictosFolder($historic[$i]->ID_SHistoric);
+            array_push($sentenceArray,$arrayProv);
+        }
+        $count = $this->HistoricInterface->getCountSentenceFolder($idusu, $idfolder);
+
+        $response = [
+            'historic' => $sentenceArray,
+            'count' => $count
+        ];
+        $this->response($response, REST_Controller::HTTP_OK);
+    }
+    
 }
--- application/models/HistoricInterface.php
+++ application/models/HistoricInterface.php
@@ -19,5 +19,79 @@ class HistoricInterface extends CI_Model {
 
         return $output;
     }
+    function getHistoric($idusu, $day){
+        $date = date('Y-m-d', strtotime("-".$day." day"));
+
+        $this->db->where('sentenceDate >', $date);
+        $this->db->where('ID_SHUser', $idusu);
+        $this->db->where('generatorString IS NOT NULL', null, false);
+        $query = $this->db->get('S_Historic');
+
+        if ($query->num_rows() > 0) {
+            $output = $query->result();
+        } else
+            $output = null;
+
+        return $output;
+    }
+    
+    function getPictosHistoric($IDHistoric){
+        $this->db->where('ID_SHistoric', $IDHistoric);
+        $this->db->join('R_S_HistoricPictograms', 'S_Historic.ID_SHistoric = R_S_HistoricPictograms.ID_RSHPSentence', 'left');
+        $this->db->join('Pictograms', 'R_S_HistoricPictograms.pictoid = Pictograms.pictoid', 'left');
+        $query = $this->db->get('S_Historic');
+        
+        if ($query->num_rows() > 0) {
+            $output = $query->result();
+        } else
+            $output = null;
+
+        return $output;
+    }
+    
+    function getCountHistoric($idusu, $day){
+        $date = date('Y-m-d', strtotime("-".$day." day"));
+        $this->db->where('sentenceDate >', $date);
+        $this->db->where('ID_SHUser', $idusu);
+        $this->db->where('generatorString IS NOT NULL', null, false);
+        $query = $this->db->get('S_Historic');
+
+        return $query->num_rows();
+    }
+    
+    function getSentenceFolder($idusu, $folder){
+        $this->db->where('ID_SSUser', $idusu);
+        $this->db->where('ID_SFolder', $folder);
+        $query = $this->db->get('S_Sentence');
+
+        if ($query->num_rows() > 0) {
+            $output = $query->result();
+        } else
+            $output = null;
+
+        return $output;
+    }
+    
+    function getCountSentenceFolder($idusu, $folder){
+        $this->db->where('ID_SSUser', $idusu);
+        $this->db->where('ID_SFolder', $folder);
+        $query = $this->db->get('S_Sentence');
+
+        return $query->num_rows();
+    }
+    
+    function getPictosFolder($IDHistoric){
+        $this->db->where('ID_SSentence', $IDHistoric);
+        $this->db->join('R_S_SentencePictograms', 'S_Sentence.ID_SSentence = R_S_SentencePictograms.ID_RSSPSentence', 'left');
+        $this->db->join('Pictograms', 'R_S_SentencePictograms.pictoid = Pictograms.pictoid', 'left');
+        $query = $this->db->get('S_Sentence');
+        
+        if ($query->num_rows() > 0) {
+            $output = $query->result();
+        } else
+            $output = null;
+
+        return $output;
+    }
 
 }
